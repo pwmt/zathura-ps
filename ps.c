e038ee7517436c7d066a66993e4622c99b29cbbf
Sebastian Ramacher <s.ramacher@gmx.at>
2011-09-29 15:34:16 +0200

    prepare for content type support

--- ps.c
+++ ps.c
@@ -7,7 +7,7 @@
 void
 plugin_register(zathura_document_plugin_t* plugin)
 {
-  plugin->file_extension = "ps";
+  girara_list_append(plugin->content_types, g_content_type_from_mime_type("application/postscript"));
   plugin->open_function  = ps_document_open;
 }
 
