8659560c73b3d1bd3012b36ff045a5a9f3f3df2b
Sebastian Ramacher <s.ramacher@gmx.at>
2012-02-21 13:06:19 +0100

    add AUTHORS file and use zathura's plugin dir

--- config.mk
+++ config.mk
@@ -14,6 +14,8 @@ SPECTRE_LIB ?= $(shell pkg-config --libs libspectre)
 
 GIRARA_INC ?= $(shell pkg-config --cflags girara-gtk2)
 ZATHURA_INC ?= $(shell pkg-config --cflags zathura)
+PLUGINDIR ?= $(shell pkg-config --variable=plugindir zathura)
+PLUGINDIR ?= ${PREFIX}/lib/zathura
 
 INCS = ${GTK_INC} ${SPECTRE_INC} ${ZATHURA_INC} ${GIRARA_INC}
 LIBS = ${GTK_LIB} ${SPECTRE_LIB}
