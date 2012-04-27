6360284d4486eace84682aa9432129b666e5cf0a
Sebastian Ramacher <s.ramacher@gmx.at>
2012-04-27 22:48:46 +0200

    Get rid of the warnings.

--- ps.c
+++ ps.c
@@ -17,15 +17,15 @@ static const char* get_extension(const char* path);
 void
 register_functions(zathura_plugin_functions_t* functions)
 {
-  functions->document_open            = ps_document_open;
-  functions->document_free            = ps_document_free;
-  functions->page_init                = ps_page_init;
-  functions->page_clear               = ps_page_clear;
-  functions->page_render              = ps_page_render;
-  functions->document_save_as         = ps_document_save_as;
-  functions->document_get_information = ps_document_get_information;
-  #if HAVE_CAIRO
-  functions->page_render_cairo        = ps_page_render_cairo;
+  functions->document_open            = (zathura_plugin_document_open_t) ps_document_open;
+  functions->document_free            = (zathura_plugin_document_free_t) ps_document_free;
+  functions->page_init                = (zathura_plugin_page_init_t) ps_page_init;
+  functions->page_clear               = (zathura_plugin_page_clear_t) ps_page_clear;
+  functions->page_render              = (zathura_plugin_page_render_t) ps_page_render;
+  functions->document_save_as         = (zathura_plugin_document_save_as_t) ps_document_save_as;
+  functions->document_get_information = (zathura_plugin_document_get_information_t) ps_document_get_information;
+#if HAVE_CAIRO
+  functions->page_render_cairo        = (zathura_plugin_page_render_cairo_t) ps_page_render_cairo;
 #endif
 }
 
