8987f250bcd0c081249b707095921003fd196c89
Sebastian Ramacher <sebastian@ramacher.at>
2026-07-08 21:29:38 +0200

    Fix version handling of plugins

--- zathura-ps/plugin.c
+++ zathura-ps/plugin.c
@@ -2,7 +2,7 @@
 
 #include "plugin.h"
 
-ZATHURA_PLUGIN_REGISTER_WITH_FUNCTIONS("ps", VERSION_MAJOR, VERSION_MINOR, VERSION_REV,
+ZATHURA_PLUGIN_REGISTER_WITH_FUNCTIONS("ps", VERSION,
                                        ZATHURA_PLUGIN_FUNCTIONS({
                                            .document_open            = ps_document_open,
                                            .document_free            = ps_document_free,
